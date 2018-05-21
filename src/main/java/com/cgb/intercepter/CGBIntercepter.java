e8fee4c9b9dd0ea54a01d73ef77ad2481e67d79a
Jonathan.Wei <252637867@qq.com>
2018-05-21 17:06:08 +0800

    init sequoiadb sink

---
+++ src/main/java/com/cgb/intercepter/CGBIntercepter.java
@@ -0,0 +1,34 @@
+package com.cgb.intercepter;
+
+import java.util.List;
+
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+public class CGBIntercepter implements Interceptor{
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Event intercept(Event event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Event> intercept(List<Event> events) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/org/apache/flume/sink/sequoiadb/Config.java
@@ -0,0 +1,33 @@
+package org.apache.flume.sink.sequoiadb;
+
+public class Config {
+	 public static final String SEQUOIADB_SERVERS = "sequoiadb.servers";
+	 public static final String SEQUOIADB_USERNAME = "sequoiadb.username";
+	 public static final String SEQUOIADB_PASSWORD = "sequoiadb.password";
+	 
+	 public static final String SEQUOIADB_POOL_CONNECTTIMEOUT = "sequoiadb.pool.connectTimeOut";
+	 public static final String SEQUOIADB_POOL_MAXAUTOCONNECTRETRYTIME = "sequoiadb.pool.maxAutoConnectRetryTime";
+	 public static final String SEQUOIADB_POOL_MAXCOUNT = "sequoiadb.pool.maxCount";
+	 public static final String SEQUOIADB_POOL_DELTAINCCOUNT = "sequoiadb.pool.deltaIncCount";
+	 public static final String SEQUOIADB_POOL_MAXIDLECOUNT = "sequoiadb.pool.maxIdleCount";
+	 public static final String SEQUOIADB_POOL_KEEPALIVETIMEOUT = "sequoiadb.pool.keepAliveTimeout";
+	 public static final String SEQUOIADB_POOL_CHECKINTERVAL = "sequoiadb.pool.checkInterval";
+	 public static final String SEQUOIADB_POOL_SYNCCOORDINTERVAL = "sequoiadb.pool.syncCoordInterval";
+	 public static final String SEQUOIADB_POOL_VALIDATECONNECTION = "sequoiadb.pool.validateConnection";
+	 
+	 public static final String SEQUOIADB_POOL_CONNECTSTRATEGY = "sequoiadb.pool.connectStrategy";
+	 
+	 public static final String SEQUOIADB_BATCHSIZSE = "sequoiadb.batchSize";
+	 public static final String SEQUOIADB_COLLECTION_SPACE = "sequoiadb.collectionSpace";
+	 public static final String SEQUOIADB_COLLECTION = "sequoiadb.collection";
+	 public static final String SEQUOIADB_COLLECTION_FIELDS = "sequoiadb.collection.fields";
+	 public static final String SEQUOIADB_COLLECTION_FIELDDELIMITER = "sequoiadb.collection.field.delimiter";
+	 public static final String SEQUOIADB_COLLECTION_FIELDTYPES = "sequoiadb.collection.field.types"; 
+	 
+	 public static final String SEQUOIADB_OPERATION_INDEX = "sequoiadb.operation.index";
+	 public static final String SEQUOIADB_UNIQUE_IDENTIFIER_INDEX = "sequoiadb.unique.identifier.index";
+	 public static final String SEQUOIADB_OPERATION_DELEMITER = "sequoiadb.operation.delimiter";
+	 public static final String SEQUOIADB_INSERT_BATCHSIZE = "sequoiadb.insert.batchSize";
+	 public static final String SEQUOIADB_OPERATION_TYPES = "sequoiadb.operation.types";
+	 
+}
